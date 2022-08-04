948c1b85dd3c57b016062dd9baf077efe924f4e6
wen <yanwen12@amd.com>
2022-08-04 10:21:55 +0800

    modify bs.py

--- bs.py
+++ bs.py
@@ -1,15 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main():
+def test():
     url = 'Unigine_Heaven_Benchmark_4.0_20220803_1402.html'
     doc = open(url, 'r', encoding='utf-8').read()
     soup = BeautifulSoup(doc, "html.parser")
-
+    # print("allText:",soup.findAll("table"))
+    my_dict=dict()
     for table in soup.findAll("table"):
         for row in table.findAll('tr'):
-            print(row.find('td').string)
-            print(row.find('td').find_next().string)
+            my_dict[row.find('td').string]=row.find('td').find_next().string
+            # print(row.find('td').string)
+            # print(row.find('td').find_next().string)
+
+    print(my_dict)
 
 
-main()
+if __name__ == '__main__':
+    test()
